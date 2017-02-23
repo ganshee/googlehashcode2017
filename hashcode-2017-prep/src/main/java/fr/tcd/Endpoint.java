195040ea8d40ce71cd6abcb0a3b70ad462000e02
Patrick Allain <patralla@gmail.com>
2017-02-23 19:15:28 +0100

    add input data

--- hashcode-2017-prep/src/main/java/fr/tcd/Endpoint.java
+++ hashcode-2017-prep/src/main/java/fr/tcd/Endpoint.java
@@ -1,14 +1,17 @@
 package fr.tcd;
 
+import java.util.Objects;
+
 public class Endpoint {
+
     public int id;
+
     public int latency;
 
     /**
      * Sets the id
-     * 
-     * @param id the id to set
      *
+     * @param id the id to set
      * @return this
      */
     public Endpoint setId(int id) {
@@ -18,9 +21,8 @@ public class Endpoint {
 
     /**
      * Sets the latency
-     * 
-     * @param latency the latency to set
      *
+     * @param latency the latency to set
      * @return this
      */
     public Endpoint setLatency(int latency) {
@@ -28,4 +30,17 @@ public class Endpoint {
         return this;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Endpoint endpoint = (Endpoint) o;
+        return id == endpoint.id &&
+                latency == endpoint.latency;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, latency);
+    }
 }
---
+++ hashcode-2017-prep/src/main/java/fr/tcd/InputData.java
@@ -0,0 +1,8 @@
+package fr.tcd;
+
+/**
+ * @author pallain - 2/23/17.
+ */
+public class InputData {
+
+}
